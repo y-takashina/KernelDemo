7bda48f5e83a7dc81e754b72fbce268d5b8ed0de
ytakashina <takashina2051@gmail.com>
2017-06-05 03:08:19 +0900

    ガウス過程のデモ

--- KernelDemo/Form1.cs
+++ KernelDemo/Form1.cs
@@ -3,18 +3,62 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace KernelDemo
 {
     public partial class Form1 : Form
     {
+        private void _plotPoints(IEnumerable<(double x, double y)> points, string legend, SeriesChartType type = SeriesChartType.Point, ChartDashStyle dashStyle = ChartDashStyle.Solid)
+        {
+            chart1.Series.Remove(chart1.Series.FindByName(legend));
+            chart1.Series.Add(legend);
+            chart1.Series[legend].ChartType = type;
+            chart1.Series[legend].BorderDashStyle = dashStyle;
+            chart1.Series[legend].BorderWidth = 2;
+            chart1.ChartAreas[0].AxisX.Minimum = -7;
+            chart1.ChartAreas[0].AxisX.Maximum = 7;
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            foreach (var point in points)
+            {
+                chart1.Series[legend].Points.AddXY(point.x, point.y);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+            chart1.Series.Clear();
+
+            var data = new List<(double x, double y)>();
+            using (var sr = new StreamReader("GaussianProcessData20121209.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',').Select(double.Parse).ToArray();
+                    data.Add((line[0], line[1]));
+                }
+            }
+            _plotPoints(data, "observed data");
+
+            var means = new List<(double x, double y)>();
+            var upper = new List<(double x, double y)>();
+            var lower = new List<(double x, double y)>();
+            for (var x = -7.0; x < 7; x += 0.1)
+            {
+                var pred = Kernel.Predict(data.ToArray(), x, Kernel.PorynomialKernel, 0.16, 1.0);
+                means.Add((x, pred.mean));
+                upper.Add((x, pred.mean + Math.Sqrt(pred.deviation)));
+                lower.Add((x, pred.mean - Math.Sqrt(pred.deviation)));
+            }
+            _plotPoints(means, "inferred function", SeriesChartType.Line);
+            _plotPoints(upper, "+1 S.D.", SeriesChartType.Line, ChartDashStyle.Dash);
+            _plotPoints(lower, "-1 S.D.", SeriesChartType.Line, ChartDashStyle.Dash);
         }
     }
 }
---
+++ KernelDemo/Kernel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Accord.Math;
+
+namespace KernelDemo
+{
+    public static class Kernel
+    {
+        public static double PorynomialKernel(double x1, double x2) => Math.Pow(x1 * x2 + 1, 3);
+        public static double LinearKernel(double x1, double x2) => x1 * x2;
+        public static double SigmoidKernel(double x1, double x2) => 1.0 / (1 + Math.Exp(-1.0 * x1 * x2));
+        public static double GaussianKernel(double x1, double x2) => Math.Exp(-0.5 / 0.25 * (x1 - x2) * (x1 - x2));
+
+
+        public static double[,] CalcCMat(IEnumerable<double> data, Func<double, double, double> kernel, double alpha, double beta)
+        {
+            var gramMatrix = data.Select(x1 => data.Select(x2 => kernel(x1, x2)).ToArray()).ToArray().ToMatrix();
+            return gramMatrix.Divide(alpha).Add(Matrix.Identity(data.Count()).Divide(beta));
+        }
+
+        public static double[] CalcCVec(IEnumerable<double> data, double newX, Func<double, double, double> kernel, double alpha)
+        {
+            return data.Select(x => kernel(x, newX)).ToArray().Divide(alpha);
+        }
+
+        public static double CalcCSca(double newX, Func<double, double, double> kernel, double alpha, double beta)
+        {
+            return kernel(newX, newX) / alpha + 1.0 / beta;
+        }
+
+        public static (double mean, double deviation) Predict((double x, double y)[] observations, double newX, Func<double, double, double> kernel, double alpha, double beta)
+        {
+            var x = observations.Select(t => t.x).ToArray();
+            var y = observations.Select(t => t.y).ToArray();
+            var cMat = CalcCMat(x, kernel, alpha, beta);
+            var cVec = CalcCVec(x, newX, kernel, alpha);
+            var cSca = CalcCSca(newX, kernel, alpha, beta);
+            var cMatInv = cMat.Inverse();
+            var mean = cVec.Dot(cMatInv).Dot(y);
+            var dev = cSca - cVec.Dot(cMatInv).Dot(cVec);
+            return (mean, dev);
+        }
+    }
+}
